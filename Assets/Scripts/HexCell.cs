85e6847a0b7c96685521a70f573d8977f6bda3c8
aith <ariiramanesh@gmail.com>
2020-10-28 00:29:28 -0700

    Initial commit

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+
+	public HexCoordinates coordinates;
+
+	public Color color;
+}
---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+	public int width = 6;
+	public int height = 6;
+
+	public Color defaultColor = Color.white;
+
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	HexCell[] cells;
+
+	Canvas gridCanvas;
+	HexMesh hexMesh;
+
+	void Awake () {
+		gridCanvas = GetComponentInChildren<Canvas>();
+		hexMesh = GetComponentInChildren<HexMesh>();
+
+		cells = new HexCell[height * width];
+
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void Start () {
+		hexMesh.Triangulate(cells);
+	}
+
+	public void ColorCell (Vector3 position, Color color) {
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = color;
+		hexMesh.Triangulate(cells);
+	}
+
+	void CreateCell (int x, int z, int i) {
+		Vector3 position;
+		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+		position.y = 0f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
+
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = cell.coordinates.ToStringOnSeparateLines();
+	}
+}
