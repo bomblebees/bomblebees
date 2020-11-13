aabf2b5380652723dc6394216456381b33f64ae9
Yooha Kim <yookakim7259@gmail.com>
2020-11-12 22:56:59 -0800

    added script file

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	[SerializeField]
+	protected int startingHealth = 100;
+
+	private int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
