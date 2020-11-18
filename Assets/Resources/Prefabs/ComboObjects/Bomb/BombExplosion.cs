7d81a471efffadc81c0060f8fd3161294d72d692
aith <ariiramanesh@gmail.com>
2020-11-17 16:00:41 -0800

    added Bomb assets

---
+++ Assets/Resources/Prefabs/ComboObjects/Bomb/BombExplosion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    private float lifeDuration = 0.5f;
+    private float sizeScalar = 2f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        ChangeScale();
+        StartCoroutine(TickDown());
+    }
+
+    private void ChangeScale()
+    {
+        this.transform.localScale *= sizeScalar;
+    }
+
+    IEnumerator TickDown()
+    {
+        yield return new WaitForSeconds(lifeDuration);
+        DestroySelf();
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Prefabs/ComboObjects/Bomb/BombObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombObject : MonoBehaviour
+{
+    private float tickDuration = 2f;
+
+    private void Awake()
+    {
+        StartCoroutine(TickDown());
+    }
+
+    IEnumerator TickDown()
+    {
+        Debug.Log(String.Concat("Bomb is ticking for ", tickDuration));
+        yield return new WaitForSeconds(tickDuration);
+
+        this.Explode();
+        this.DestroySelf();
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Explode()
+    {
+        Debug.Log("Creating Bomb Explosion");
+        new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        if (!Instantiate(Resources.Load("Prefabs/ComboObjects/Bomb/Bomb Explosion"),
+            new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z),
+            Quaternion.identity))
+        {
+            Debug.LogError("Could not instantiate Bomb Explosion");
+        }
+    }
+}
