721e6fc03c4b7a4f46b736c728b6901c492f07a6
Yooha Kim <yookakim7259@gmail.com>
2020-11-28 15:26:58 -0800

    Set up simple event-based UI

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,6 +1,7 @@
 ﻿// Referencing Jason Weimann's YouTube video "Unity Architecture - Composition or Inheritance?"
 // https://www.youtube.com/watch?v=8TIkManpEu4
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,23 @@ public class Health : MonoBehaviour
 	[SerializeField]
 	protected int startingHealth = 100;
 
-	private int health;
+	private int currentHealth;
+
+	// read only (is this the right way to do this lol?)
+	public int CurrentHealth
+	{
+		get { return currentHealth; }
+		set { }
+	}
+
+	// https://learn.unity.com/tutorial/c-actions
+	public event Action<GameObject> Died;
+	public event Action<int> HealthChanged;
 
     // Start is called before the first frame update
     void Awake()
     {
-		health = startingHealth;
+		currentHealth = startingHealth;
     }
 
     // Update is called once per frame
@@ -26,24 +38,29 @@ public class Health : MonoBehaviour
 
 	public void DealDamage(int amount)
 	{
-		Debug.Log(health);
-		if (health > 0)
+		Debug.Log(currentHealth);
+		if (currentHealth > 0)
 		{
-			health -= amount;
+			// change currentHealth by amount (and emit damage dealt event)
+			currentHealth -= amount;
+			HealthChanged?.Invoke(currentHealth);
 		}
-		if (health <= 0)
+
+		if (currentHealth <= 0)
 		{
+			// call method for when currentHealth hits 0 (and emit death event)
 			OnHealthZero();
 		}
 	}
 
 	private void OnHealthZero()
 	{
-		Debug.Log("health under or at 0");
-		Debug.Log(health);
-
+		Died?.Invoke(gameObject);
+		
 		// For now, just delete the gameobject this script is stuck to
 		// Later we might use events for game state stuff, so that functionality can be split into more intuitive ways
-		Destroy(gameObject);
+		
+		
+		//Destroy(gameObject);
 	}
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	// Start is called before the first frame update
+	[SerializeField]
+	private Text Player1HealthText;
+
+	// store reference into property at startup, so we can listen to its events
+	private GameObject Player;
+
+	private void OnEnable()
+	{
+
+		
+
+	}
+
+	private void OnDisable()
+	{
+		// problem: results in error if we disable after the player is dead/gameobject is deleted
+		Player.GetComponent<Health>().HealthChanged -= UpdateUI;
+	}
+
+	void Start()
+    {
+
+		// to find the Player object in runtime we wait until Start and then search the scene:
+		Player = GameObject.FindWithTag("Player");
+
+
+		// searching and finding specific object in scene with FindWithTag is slow
+		// so later we can search an array of players managed by
+		// some sort of manager instead of looking for it by tag? idk
+		Player.GetComponent<Health>().HealthChanged += UpdateUI;
+		Debug.Log(Player.name);
+
+		Debug.Log("P1 Health: " + Player.GetComponent<Health>().CurrentHealth);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void UpdateUI(int newHealthAmount)
+	{
+		// Runs when the change health event is invoked in our Health script
+		// Update the p1 health text we dragged in through the inspector
+		Debug.Log("new HP amount is " + newHealthAmount);
+		Player1HealthText.text = "P1 Health: " + newHealthAmount;
+	}
+}
