5faebd3035ca0194485c81575abdf07d891360fb
terencehso <51182302+terencehso@users.noreply.github.com>
2021-01-20 19:20:12 -0800

    added laser vfx 1st pass

--- Assets/Scripts/ComboObjects/LaserObject.cs
+++ Assets/Scripts/ComboObjects/LaserObject.cs
@@ -18,5 +18,9 @@ public class LaserObject : TriggerObject
     {
         Debug.Log("edgeIndex is "+edgeIndex);
         this.gameObject.GetComponent<Transform>().transform.Find("Hitbox").transform.eulerAngles = new Vector3(90f, 0f, -HexMetrics.edgeAngles[edgeIndex]);
+
+        // V this is kinda busted, 4/6 angles are wrong
+        this.gameObject.GetComponent<Transform>().transform.Find("VFX").transform.eulerAngles += new Vector3(0f, 0f, HexMetrics.edgeAngles[edgeIndex]+270f);
+
     }
 }
