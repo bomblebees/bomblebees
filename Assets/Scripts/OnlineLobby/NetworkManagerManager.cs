575f85d4ba25f2d1f6ee2a08debded5f598a97ad
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-01-26 09:53:53 +0700

    New Scene Version: Level1.2 -add steam network option -add online lobby (still broken)

---
+++ Assets/Scripts/OnlineLobby/NetworkManagerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror.Authenticators;
+using UnityEngine;
+
+public class NetworkManagerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject networkManagerManager;
+    
+    [SerializeField] private GameObject basicNetworkManager;
+    [SerializeField] private GameObject steamNetworkManager;
+    [SerializeField] private GameObject kcpNetworkManager;
+    public void ChooseBasicNetworkManager()
+    {
+        networkManagerManager.SetActive(false);
+        basicNetworkManager.SetActive(true);
+    }
+    
+    public void ChooseSteamNetworkManager()
+    {
+        networkManagerManager.SetActive(false);
+        steamNetworkManager.SetActive(true);
+    }
+
+    public void ChooseKcpNetworkManager()
+    {
+        networkManagerManager.SetActive(false);
+        kcpNetworkManager.SetActive(true);
+    }
+}
