aadaa6a40ba88e460d152eda9b93a371cf186323
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-02-21 18:05:59 +0700

    Update UI & Add some UI Assets -change menu background -add in-game "options" menu -add "how to play" button and popup -add GUI PRO Kit - Casual Game 1.3.4 for reference: https://assetstore.unity.com/packages/2d/gui/gui-pro-kit-casual-game-176695 -add Polyverse Skies • Low poly skybox shaders and textures 1.6.0 for reference: https://assetstore.unity.com/packages/vfx/shaders/polyverse-skies-low-poly-skybox-shaders-and-textures-104017

---
+++ Assets/Scenes/Scripts/Networking/Other Scripts/InGame_UI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InGame_UI : MonoBehaviour
+{
+    [SerializeField] private NetworkManager networkManager;
+    [SerializeField] [Scene] private string RoomScene;
+    [SerializeField] private GameObject screenOptions;
+    
+    void Start()
+    {
+        networkManager = FindObjectOfType<NetworkManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            ToggleScreenOptions();
+        }
+    }
+
+    #region Screen: Options
+
+    public void ToggleScreenOptions()
+    {
+        screenOptions.SetActive(!screenOptions.activeSelf);
+    }
+    
+    public void ReturnToRoom()
+    {
+        networkManager.ServerChangeScene(RoomScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scenes/Scripts/Networking/Other Scripts/Room_UI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Steamworks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Room_UI : MonoBehaviour
+{
+    [SerializeField] private NetworkManager networkManager;
+    [SerializeField] private MainMenu_UI mainMenuUI;
+    [SerializeField] private GameObject screenHowToPlay;
+
+    public void Start()
+    {
+        networkManager = FindObjectOfType<NetworkManager>();
+        mainMenuUI = FindObjectOfType<MainMenu_UI>();
+        
+        mainMenuUI.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            networkManager.StopHost();
+        }
+        // stop client if client-only
+        else if (NetworkClient.isConnected)
+        {
+            networkManager.StopClient();
+        }
+        // stop server if server-only
+        else if (NetworkServer.active)
+        {
+            networkManager.StopServer();
+        }
+        
+        SceneManager.LoadScene("MainMenu");
+        mainMenuUI.gameObject.SetActive(true);
+    }
+
+    #region Screen: HOW TO PLAY
+
+    public void ToggleScreenHowToPlay()
+    {
+        screenHowToPlay.SetActive(!screenHowToPlay.activeSelf);
+    }
+
+    #endregion
+}
