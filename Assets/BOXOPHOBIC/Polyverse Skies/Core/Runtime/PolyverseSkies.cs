92486d44b1494d61e38cdcf6ffd0580f4cf57745
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-04-05 03:30:48 +0700

    Update Polyverse Skies -update to Polyverse Skies • Low poly skybox shaders and textures 1.6.1 for reference: https://assetstore.unity.com/packages/vfx/shaders/polyverse-skies-low-poly-skybox-shaders-and-textures-104017

--- Assets/BOXOPHOBIC/Polyverse Skies/Core/Runtime/PolyverseSkies.cs
+++ Assets/BOXOPHOBIC/Polyverse Skies/Core/Runtime/PolyverseSkies.cs
@@ -14,12 +14,12 @@ public class PolyverseSkies : StyledMonoBehaviour
     public bool categoryScene;
 
     public GameObject sunDirection;
-	public GameObject moonDirection;
+    public GameObject moonDirection;
 
     [StyledCategory("Time Of Day")]
     public bool categoryTime;
 
-    [StyledMessage("Info", "The Time Of Day feature will interpolate between two Polyverse Skies materials. Please note that not all material properties such as textures and keywords will not be interpolated! Toggle Update Lighting to enable Unity's realtime environment lighting!", 0, 10)]
+    [StyledMessage("Info", "The Time Of Day feature will interpolate between two Polyverse Skies materials. Please note that material properties such as textures and keywords will not be interpolated! You will need to enable the same features on both materials in order for the interpolation to work! Toggle Update Lighting to enable Unity's realtime environment lighting! ", 0, 10)]
     public bool categoryTimeMessage = true;
 
     public Material skyboxDay;
@@ -33,29 +33,38 @@ public class PolyverseSkies : StyledMonoBehaviour
     [StyledSpace(5)]
     public bool styledSpace0;
 
-	void Update ()
-    {
-		if (sunDirection != null)
-        {
-			Shader.SetGlobalVector ("GlobalSunDirection", -sunDirection.transform.forward);
-		}
-        else
-        {
-			Shader.SetGlobalVector ("GlobalSunDirection", Vector3.zero);
-		}
+    Material skyboxMaterial;
 
-		if (moonDirection != null)
+    void Start()
+    {
+        if (skyboxDay != null && skyboxNight != null)
         {
-			Shader.SetGlobalVector ("GlobalMoonDirection", -moonDirection.transform.forward);
-		}
+            skyboxMaterial = new Material(skyboxDay);
+        }
+    }
+
+    void Update()
+    {
+        if (sunDirection != null)
+        {
+            Shader.SetGlobalVector("GlobalSunDirection", -sunDirection.transform.forward);
+        }
         else
         {
-			Shader.SetGlobalVector ("GlobalMoonDirection", Vector3.zero);
-		}
+            Shader.SetGlobalVector("GlobalSunDirection", Vector3.zero);
+        }
+
+        if (moonDirection != null)
+        {
+            Shader.SetGlobalVector("GlobalMoonDirection", -moonDirection.transform.forward);
+        }
+        else
+        {
+            Shader.SetGlobalVector("GlobalMoonDirection", Vector3.zero);
+        }
 
         if (skyboxDay != null && skyboxNight != null)
         {
-            Material skyboxMaterial = new Material(skyboxDay);
             skyboxMaterial.Lerp(skyboxDay, skyboxNight, timeOfDay);
             RenderSettings.skybox = skyboxMaterial;
         }
@@ -65,4 +74,14 @@ public class PolyverseSkies : StyledMonoBehaviour
             DynamicGI.UpdateEnvironment();
         }
     }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (skyboxDay != null && skyboxNight != null)
+        {
+            skyboxMaterial = new Material(skyboxDay);
+        }
+    }
+#endif
 }
