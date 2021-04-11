341d517e0857193ae39b8a8fc6e1b84286800008
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-04-12 03:52:53 +0700

    Initialize Global Settings -remove local settings -reorganize Preload.unity

---
+++ Assets/ScenelessObjectManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScenelessObjectManager : MonoBehaviour
+{
+    [SerializeField] private GlobalSettings globalSettings;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            ToggleSettingsScreen();
+        }
+    }
+
+    public void ToggleSettingsScreen()
+    {
+        globalSettings.gameObject.SetActive(!globalSettings.gameObject.activeSelf);
+    }
+}
