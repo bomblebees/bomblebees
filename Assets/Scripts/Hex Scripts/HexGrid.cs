a5209880e885866d61e69260da315e2cd29789fa
wchunl <44006254+wchunl@users.noreply.github.com>
2021-04-18 15:46:21 -0700

    build errors fixed

--- Assets/Scripts/Hex Scripts/HexGrid.cs
+++ Assets/Scripts/Hex Scripts/HexGrid.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Data.Util;
 using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
