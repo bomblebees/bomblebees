36f0324747c0d6b25d9b6bfb8d0fefed2d5a8f9c
wchunl <44006254+wchunl@users.noreply.github.com>
2021-04-21 19:33:14 -0700

    added round timer

---
+++ Assets/Scripts/UserInterface/GameOverlayUI/RoundTimer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class RoundTimer : MonoBehaviour
+{
+    [Header("Start Round")]
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private Image timerRing;
+
+    private bool timerStarted = false;
+    private float roundDuration = 0f;
+    private float currentTime = 0f;
+
+    private void Update()
+    {
+        if (timerStarted)
+        {
+            if (currentTime > roundDuration)
+            {
+                Debug.Log("round finished");
+                timerStarted = false;
+            }
+
+            timerText.text = (roundDuration - currentTime).ToString("F1");
+            timerRing.fillAmount = 1 - (currentTime / roundDuration);
+
+            currentTime += Time.deltaTime;
+            //Debug.Log(currentTime);
+        }
+    }
+
+    public void StartTimer(float duration)
+    {
+        roundDuration = duration;
+        timerStarted = true;
+    }
+
+    public IEnumerator InitTimer(float duration, float freezeTime)
+    {
+        timerText.text = duration.ToString("F1");
+        yield return new WaitForSeconds(freezeTime + 1);
+        StartTimer(duration);
+    }
+}
