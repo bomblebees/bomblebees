fa19fbed60a92613e59340ae904a30bc65f59ada
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-04-26 05:49:11 +0700

    Revert "Fix character selection sync"
    This reverts commit 6f5c5d9f

--- Assets/CharacterSelectionInfo.cs
+++ Assets/CharacterSelectionInfo.cs
@@ -1,4 +1,6 @@
-﻿using Mirror;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
 public class CharacterSelectionInfo : NetworkBehaviour
@@ -22,49 +24,25 @@ public class CharacterSelectionInfo : NetworkBehaviour
 
     public void OnEventTogglePlayer1()
     {
-        CmdTogglePlayer1();
+        player1 = (player1 + 1) % 4;
         EventTogglePlayer1?.Invoke();
     }
     
     public void OnEventTogglePlayer2()
     {
-        CmdTogglePlayer2();
+        player2 = (player2 + 1) % 4;
         EventTogglePlayer2?.Invoke();
     }
     
     public void OnEventTogglePlayer3()
     {
-        CmdTogglePlayer3();
+        player3 = (player3 + 1) % 4;
         EventTogglePlayer3?.Invoke();
     }
     
     public void OnEventTogglePlayer4()
     {
-        CmdTogglePlayer4();
-        EventTogglePlayer4?.Invoke();
-    }
-
-    [Command]
-    private void CmdTogglePlayer1()
-    {
-        player1 = (player1 + 1) % 4;
-    }
-    
-    [Command]
-    private void CmdTogglePlayer2()
-    {
-        player2 = (player2 + 1) % 4;
-    }
-    
-    [Command]
-    private void CmdTogglePlayer3()
-    {
-        player3 = (player3 + 1) % 4;
-    }
-    
-    [Command]
-    private void CmdTogglePlayer4()
-    {
         player4 = (player4 + 1) % 4;
+        EventTogglePlayer4?.Invoke();
     }
 }
