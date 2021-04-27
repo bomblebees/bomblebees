de091a22e50cd373157fec9670b4444bf39c9e78
wchunl <44006254+wchunl@users.noreply.github.com>
2021-04-27 02:55:32 -0700

    added version number to main menu

---
+++ Assets/VersionTag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionTag : MonoBehaviour
+{
+    [SerializeField] private TMP_Text versionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        versionText.text = "v" + Application.version;
+    }
+}
