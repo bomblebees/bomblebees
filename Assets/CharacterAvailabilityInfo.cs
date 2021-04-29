76575c3ab76c32a349580060720eb8bb13646119
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-04-29 14:41:06 +0700

    Lock character on ready & disable duplicate characters 四

--- Assets/CharacterAvailabilityInfo.cs
+++ Assets/CharacterAvailabilityInfo.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 
 public class CharacterAvailabilityInfo : NetworkBehaviour
 {
-    [SyncVar] public bool character1 = true, character2 = true, character3 = true, character4 = true;
+    [SyncVar(hook = nameof(CharacterAvailabilityChanged))] 
+    public bool character1 = true, character2 = true, character3 = true, character4 = true;
+    
+    public void CharacterAvailabilityChanged(bool oldCharacterAvailability, bool newCharacterAvailability) { }
 }
