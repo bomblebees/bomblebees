5d7000047068479fe72a91484c4540656a301a71
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-04 04:26:28 +0700

    Add a global setting button

--- Assets/ScenelessObjectManager.cs
+++ Assets/ScenelessObjectManager.cs
@@ -2,11 +2,26 @@
 
 public class ScenelessObjectManager : MonoBehaviour
 {
+    [Header("Configure")] 
+    [SerializeField] private float settingButtonOpacity = 1f;
+    [Header("Others")]
     [SerializeField] private GlobalSettings globalSettings;
+    [SerializeField] private GameObject settingButton;
+    private CanvasRenderer[] _canvasRenderers;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _canvasRenderers = settingButton.GetComponentsInChildren<CanvasRenderer>();
+
+        foreach (var canvasRenderer in _canvasRenderers)
+        {
+            canvasRenderer.SetAlpha(settingButtonOpacity);
+        }
     }
 
     private void Update()
