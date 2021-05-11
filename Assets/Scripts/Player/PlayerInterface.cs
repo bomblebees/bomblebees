54ef9bb72d24955b4e816730dc9c20bacd2cd587
yookakim <yookakim7259@gmail.com>
2021-05-11 03:26:58 -0700

    2-tick spin and inventory UI changes

--- Assets/Scripts/Player/PlayerInterface.cs
+++ Assets/Scripts/Player/PlayerInterface.cs
@@ -149,7 +149,7 @@ public class PlayerInterface : NetworkBehaviour
     {
         float[] spinTimes = player.GetComponent<PlayerSpin>().spinTimings;
         spinChargeTime += Time.deltaTime;
-        spinChargeBar.fillAmount = spinChargeTime / spinTimes[spinTimes.Length - 2];
+        spinChargeBar.fillAmount = spinChargeTime / spinTimes[spinTimes.Length - 3];
     }
 
     public void OnPlayerTakeDamage(int currentHealth, int _, GameObject __)
