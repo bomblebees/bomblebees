c2fed84531e60934d6396d763c695e5a597b4541
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-17 01:54:49 +0700

    Add InControl from Unity Asset Store
    - Add InControl 1.8.4
    - for reference: https://assetstore.unity.com/packages/tools/input-management/incontrol-14695

---
+++ Assets/InControl/Source/Device/InputDeviceStyle.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable InconsistentNaming
+namespace InControl
+{
+	// Not all of these are necessarily used or supported.
+	// Some are simply included for future use.
+	//
+	public enum InputDeviceStyle : int
+	{
+		Unknown = 0,
+		Xbox360,
+		XboxOne,
+		XboxSeriesX,
+		PlayStation2,
+		PlayStation3,
+		PlayStation4,
+		PlayStation5,
+		PlayStationVita,
+		PlayStationMove,
+		Ouya,
+		Steam,
+		AppleMFi,
+		AmazonFireTV,
+		NVIDIAShield,
+		NintendoNES,
+		NintendoSNES,
+		Nintendo64,
+		NintendoGameCube,
+		NintendoWii,
+		NintendoWiiU,
+		NintendoSwitch,
+		GoogleStadia,
+		Vive,
+		Oculus,
+	}
+}
---
+++ Assets/InControl/Source/Unity/DeviceProfiles/Android/PlayStation4AndroidXperiaZ5UnityProfile.cs
@@ -0,0 +1,2 @@
+// TODO: Remove this after a while (2020-05-18)
+
