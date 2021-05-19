97741c6227f0e17750178681fc7df94e19ea0409
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-19 08:36:00 +0700

    Add a dedicated debug cheats object & script

---
+++ Assets/DebugCheats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DebugCheats : MonoBehaviour
+{
+    private void Update()
+    {
+        if (!Input.GetKey(KeyCode.C)) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+        }
+    }
+}
