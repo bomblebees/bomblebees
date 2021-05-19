524f8aced529e0dc16bd446953b71b6acc1ed8f2
yookakim <yookakim7259@gmail.com>
2021-05-18 19:10:51 -0700

    player HUD wip

--- Assets/Scripts/UserInterface/PlayerInterface/InventoryRadialSlottedFrame.cs
+++ Assets/Scripts/UserInterface/PlayerInterface/InventoryRadialSlottedFrame.cs
@@ -7,11 +7,23 @@ public class InventoryRadialSlottedFrame : MonoBehaviour
 {
 	public Image slottedFrameImage;
 
+	[Tooltip("Scale for the generic frame without the bomb/deployable icons")]
+	[SerializeField] private Vector2 genericFrameSize;
+	[Tooltip("Scale for the frame with the bomb/deployable bits attached")]
+	[SerializeField] private Vector2 embellishedFrameSize;
+
 	public Sprite threeSlottedFrameSprite;
 
 	public Sprite fourSlottedFrameSprite;
 
 	public Sprite fiveSlottedFrameSprite;
+
+	public Sprite threeSlottedBombFrame;
+	public Sprite fourSlottedBombFrame;
+	public Sprite fiveSlottedBombFrame;
+	public Sprite threeSlottedDeployableFrame;
+	public Sprite fourSlottedDeployableSprite;
+	public Sprite fiveSlottedDeployableSprite;
 
 	/// <summary>
 	/// Swaps the UI frame of this element to the respective sprite
@@ -19,15 +31,19 @@ public class InventoryRadialSlottedFrame : MonoBehaviour
 	/// <param name="slotSize"></param>
 	public void SwapFrame(int slotSize)
 	{
+		RectTransform rect = GetComponent<RectTransform>();
 		switch (slotSize)
 		{
 			case 3:
+				rect.sizeDelta = genericFrameSize;
 				slottedFrameImage.sprite = threeSlottedFrameSprite;
 				break;
 			case 4:
+				rect.sizeDelta = genericFrameSize;
 				slottedFrameImage.sprite = fourSlottedFrameSprite;
 				break;
 			case 5:
+				rect.sizeDelta = genericFrameSize;
 				slottedFrameImage.sprite = fiveSlottedFrameSprite;
 				break;
 			default:
