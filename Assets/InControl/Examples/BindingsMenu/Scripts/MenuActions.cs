f93fbcf2f9aeb7ebe6875d64e5dd6c52c916f526
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-23 03:13:10 +0700

    Enable controller input

--- Assets/InControl/Examples/BindingsMenu/Scripts/MenuActions.cs
+++ Assets/InControl/Examples/BindingsMenu/Scripts/MenuActions.cs
@@ -8,64 +8,64 @@
 // they can fix or reset the game controls.
 public class MenuActions : PlayerActionSet
 {
-	public readonly PlayerAction Up;
-	public readonly PlayerAction Down;
-	public readonly PlayerAction Left;
-	public readonly PlayerAction Right;
-	public readonly PlayerTwoAxisAction Move;
+    public readonly PlayerAction Up;
+    public readonly PlayerAction Down;
+    public readonly PlayerAction Left;
+    public readonly PlayerAction Right;
+    public readonly PlayerTwoAxisAction Move;
 
-	public readonly PlayerAction Submit;
-	public readonly PlayerAction Cancel;
+    public readonly PlayerAction Submit;
+    public readonly PlayerAction Cancel;
 
-	// We don't actually use this in the example, but likely there is a
-	// control to trigger the menu while in game.
-	// public readonly PlayerAction OpenMenu;
-
-
-	public MenuActions()
-	{
-		Up = CreatePlayerAction( "Move Up" );
-		Down = CreatePlayerAction( "Move Down" );
-		Left = CreatePlayerAction( "Move Left" );
-		Right = CreatePlayerAction( "Move Right" );
-		Move = CreateTwoAxisPlayerAction( Left, Right, Down, Up );
-
-		Submit = CreatePlayerAction( "Submit" );
-		Cancel = CreatePlayerAction( "Cancel" );
-
-		// OpenMenu = CreatePlayerAction( "OpenMenu" );
-	}
+    // We don't actually use this in the example, but likely there is a
+    // control to trigger the menu while in game.
+    // public readonly PlayerAction OpenMenu;
 
 
-	public static MenuActions CreateWithDefaultBindings()
-	{
-		var actions = new MenuActions();
+    public MenuActions()
+    {
+        Up = CreatePlayerAction("Move Up");
+        Down = CreatePlayerAction("Move Down");
+        Left = CreatePlayerAction("Move Left");
+        Right = CreatePlayerAction("Move Right");
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
 
-		actions.Up.AddDefaultBinding( Key.UpArrow );
-		actions.Down.AddDefaultBinding( Key.DownArrow );
-		actions.Left.AddDefaultBinding( Key.LeftArrow );
-		actions.Right.AddDefaultBinding( Key.RightArrow );
+        Submit = CreatePlayerAction("Submit");
+        Cancel = CreatePlayerAction("Cancel");
 
-		actions.Left.AddDefaultBinding( InputControlType.LeftStickLeft );
-		actions.Right.AddDefaultBinding( InputControlType.LeftStickRight );
-		actions.Up.AddDefaultBinding( InputControlType.LeftStickUp );
-		actions.Down.AddDefaultBinding( InputControlType.LeftStickDown );
+        // OpenMenu = CreatePlayerAction("OpenMenu");
+    }
 
-		actions.Left.AddDefaultBinding( InputControlType.DPadLeft );
-		actions.Right.AddDefaultBinding( InputControlType.DPadRight );
-		actions.Up.AddDefaultBinding( InputControlType.DPadUp );
-		actions.Down.AddDefaultBinding( InputControlType.DPadDown );
 
-		actions.Submit.AddDefaultBinding( Key.Return );
-		actions.Submit.AddDefaultBinding( Key.Space );
-		actions.Submit.AddDefaultBinding( InputControlType.Action1 );
+    public static MenuActions CreateWithDefaultBindings()
+    {
+        var actions = new MenuActions();
 
-		actions.Cancel.AddDefaultBinding( Key.Escape );
-		actions.Cancel.AddDefaultBinding( InputControlType.Action2 );
+        actions.Up.AddDefaultBinding(Key.UpArrow);
+        actions.Down.AddDefaultBinding(Key.DownArrow);
+        actions.Left.AddDefaultBinding(Key.LeftArrow);
+        actions.Right.AddDefaultBinding(Key.RightArrow);
 
-		// actions.OpenMenu.AddDefaultBinding( Key.Escape );
-		// actions.OpenMenu.AddDefaultBinding( InputControlType.Command );
+        actions.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+        actions.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+        actions.Up.AddDefaultBinding(InputControlType.LeftStickUp);
+        actions.Down.AddDefaultBinding(InputControlType.LeftStickDown);
 
-		return actions;
-	}
+        actions.Left.AddDefaultBinding(InputControlType.DPadLeft);
+        actions.Right.AddDefaultBinding(InputControlType.DPadRight);
+        actions.Up.AddDefaultBinding(InputControlType.DPadUp);
+        actions.Down.AddDefaultBinding(InputControlType.DPadDown);
+
+        actions.Submit.AddDefaultBinding(Key.Return);
+        actions.Submit.AddDefaultBinding(Key.Space);
+        actions.Submit.AddDefaultBinding(InputControlType.Action1);
+
+        actions.Cancel.AddDefaultBinding(Key.Escape);
+        // actions.Cancel.AddDefaultBinding(InputControlType.Action2);
+
+        // actions.OpenMenu.AddDefaultBinding(Key.Escape);
+        // actions.OpenMenu.AddDefaultBinding(InputControlType.Command);
+
+        return actions;
+    }
 }
--- Assets/InControl/Examples/BindingsMenu/Scripts/MenuManager.cs
+++ Assets/InControl/Examples/BindingsMenu/Scripts/MenuManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using InControl;
 
@@ -10,137 +9,137 @@ using InControl;
 // would likely be somewhat more complex.
 public class MenuManager : MonoBehaviour
 {
-	public MenuActions MenuActions { get; private set; }
-	public GameActions GameActions { get; private set; }
+    public MenuActions MenuActions { get; private set; }
+    public GameActions GameActions { get; private set; }
 
-	public BindingsMenu BindingsMenu;
+    public BindingsMenu BindingsMenu;
 
-	private string _gameActionsSaveData;
-	private string _menuActionsSaveData;
-
-
-	void Awake()
-	{
-		InputManager.OnSetup -= SetupInput;
-		InputManager.OnSetup += SetupInput;
-
-		InputManager.OnReset -= ResetInput;
-		InputManager.OnReset += ResetInput;
-	}
+    private string _gameActionsSaveData;
+    private string _menuActionsSaveData;
 
 
-	void SetupInput()
-	{
-		// Create the action sets for our menu and game controls.
-		MenuActions = MenuActions.CreateWithDefaultBindings();
-		GameActions = GameActions.CreateWithDefaultBindings();
-		
-		LoadBindings();
+    void Awake()
+    {
+        InputManager.OnSetup -= SetupInput;
+        InputManager.OnSetup += SetupInput;
 
-		// Get the InControlInputModule, and attach menu navigation controls.
-		var inputModule = EventSystem.current.GetComponent<InControlInputModule>();
-		Debug.Assert( inputModule != null, "inputModule != null" );
-		if (inputModule != null)
-		{
-			inputModule.MoveAction = MenuActions.Move;
-			inputModule.SubmitAction = MenuActions.Submit;
-			inputModule.CancelAction = MenuActions.Cancel;
-		}
-	}
-
-	public void ShowGameActions()
-	{
-		BindingsMenu.Hide();
-		BindingsMenu.Show( GameActions );
-	}
+        InputManager.OnReset -= ResetInput;
+        InputManager.OnReset += ResetInput;
+    }
 
 
-	void ResetInput()
-	{
-		if (MenuActions != null)
-		{
-			MenuActions.Destroy();
-			MenuActions = null;
-		}
+    void SetupInput()
+    {
+        // Create the action sets for our menu and game controls.
+        MenuActions = MenuActions.CreateWithDefaultBindings();
+        GameActions = GameActions.CreateWithDefaultBindings();
 
-		if (GameActions != null)
-		{
-			GameActions.Destroy();
-			GameActions = null;
-		}
-	}
+        LoadBindings();
 
+        // Get the InControlInputModule, and attach menu navigation controls.
+        var inputModule = EventSystem.current.GetComponent<InControlInputModule>();
+        Debug.Assert(inputModule != null, "inputModule != null");
+        if (inputModule != null)
+        {
+            inputModule.MoveAction = MenuActions.Move;
+            inputModule.SubmitAction = MenuActions.Submit;
+            inputModule.CancelAction = MenuActions.Cancel;
+        }
+    }
 
-	#region Basic singleton implementation.
-
-	static MenuManager instance;
-	static bool applicationIsQuitting;
-	static readonly object lockObject = new object();
-
-
-	// This project uses the experimental fast enter play mode options with domain reloading disabled,
-	// so we need to reinitialize our static variables.
-	// https://docs.unity3d.com/Manual/DomainReloading.html
-	[RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.SubsystemRegistration )]
-	static void Init()
-	{
-		instance = null;
-		applicationIsQuitting = false;
-	}
+    public void ShowGameActions()
+    {
+        BindingsMenu.Hide();
+        BindingsMenu.Show(GameActions);
+    }
 
 
-	void OnApplicationQuit()
-	{
-		PlayerPrefs.Save();
-		applicationIsQuitting = true;
-		instance = null;
-	}
-	
-	public void SaveBindings()
-	{
-		_gameActionsSaveData = GameActions.Save();
-		PlayerPrefs.SetString( "Bindings: Game Actions", _gameActionsSaveData );
+    void ResetInput()
+    {
+        if (MenuActions != null)
+        {
+            MenuActions.Destroy();
+            MenuActions = null;
+        }
 
-		_menuActionsSaveData = MenuActions.Save();
-		PlayerPrefs.SetString( "Bindings: Menu Actions", _menuActionsSaveData );
-	}
+        if (GameActions != null)
+        {
+            GameActions.Destroy();
+            GameActions = null;
+        }
+    }
 
 
-	private void LoadBindings()
-	{
-		if (PlayerPrefs.HasKey( "Bindings: Game Actions" ))
-		{
-			_gameActionsSaveData = PlayerPrefs.GetString( "Bindings: Game Actions" );
-			GameActions.Load( _gameActionsSaveData );
-		}
-		
-		if (PlayerPrefs.HasKey( "Bindings: Menu Actions" ))
-		{
-			_menuActionsSaveData = PlayerPrefs.GetString( "Bindings: Menu Actions" );
-			MenuActions.Load( _menuActionsSaveData );
-		}
-	}
+    #region Basic singleton implementation.
 
-	public static MenuManager Instance
-	{
-		get
-		{
-			if (applicationIsQuitting)
-			{
-				return null;
-			}
+    static MenuManager instance;
+    static bool applicationIsQuitting;
+    static readonly object lockObject = new object();
 
-			lock (lockObject)
-			{
-				if (instance == null)
-				{
-					instance = FindObjectOfType( typeof(MenuManager) ) as MenuManager;
-				}
 
-				return instance;
-			}
-		}
-	}
+    // This project uses the experimental fast enter play mode options with domain reloading disabled,
+    // so we need to reinitialize our static variables.
+    // https://docs.unity3d.com/Manual/DomainReloading.html
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    static void Init()
+    {
+        instance = null;
+        applicationIsQuitting = false;
+    }
 
-	#endregion
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.Save();
+        applicationIsQuitting = true;
+        instance = null;
+    }
+
+    public void SaveBindings()
+    {
+        _gameActionsSaveData = GameActions.Save();
+        PlayerPrefs.SetString("Bindings: Game Actions", _gameActionsSaveData);
+
+        _menuActionsSaveData = MenuActions.Save();
+        PlayerPrefs.SetString("Bindings: Menu Actions", _menuActionsSaveData);
+    }
+
+
+    private void LoadBindings()
+    {
+        if (PlayerPrefs.HasKey("Bindings: Game Actions"))
+        {
+            _gameActionsSaveData = PlayerPrefs.GetString("Bindings: Game Actions");
+            GameActions.Load(_gameActionsSaveData);
+        }
+
+        // if (PlayerPrefs.HasKey("Bindings: Menu Actions"))
+        // {
+        // 	_menuActionsSaveData = PlayerPrefs.GetString("Bindings: Menu Actions");
+        // 	MenuActions.Load(_menuActionsSaveData);
+        // }
+    }
+
+    public static MenuManager Instance
+    {
+        get
+        {
+            if (applicationIsQuitting)
+            {
+                return null;
+            }
+
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType(typeof(MenuManager)) as MenuManager;
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    #endregion
 }
