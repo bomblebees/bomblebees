c53c4b1f5a8c13b27aaac21306a39e99303757a7
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-26 02:50:24 +0700

    Add practice mode button

---
+++ Assets/PracticeButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PracticeButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        FindObjectOfType<Matchmaking>().CreateLobby();
+        InvokeRepeating(nameof(TryToStartGame), Single.Epsilon, 0.1f);
+    }
+
+    private void TryToStartGame()
+    {
+        if (FindObjectOfType<NetworkRoomPlayerExt>() is null) return;
+        
+        if (FindObjectOfType<LobbySettings>() is null || FindObjectOfType<NetworkRoomManagerExt>() is null)
+        {
+            Debug.LogWarning("This should never be called");
+            return;
+        }
+        
+        CancelInvoke(nameof(TryToStartGame));
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        var lobbySettings = FindObjectOfType<LobbySettings>();
+        lobbySettings.roundDuration = 0f;
+        lobbySettings.playerLives = 0;
+        
+        var networkRoomManagerExt = FindObjectOfType<NetworkRoomManagerExt>();
+        networkRoomManagerExt.ServerChangeScene(networkRoomManagerExt.GameplayScene);
+    }
+}
