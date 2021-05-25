578bf63f0052aece74cdcc98f49c75583ed451ea
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-26 04:49:53 +0700

    Add finish practice button

---
+++ Assets/FinishPracticeButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FinishPracticeButton : MonoBehaviour
+{
+    private void Awake()
+    { 
+        GetComponentInChildren<Canvas>().enabled = FindObjectOfType<PingDisplay>().myPingDisplay.Equals("Practice Mode");
+    }
+
+    public void FinishPractice()
+    {
+        FindObjectOfType<GlobalSettings>().OnClickQuitToMenu();
+    }
+}
