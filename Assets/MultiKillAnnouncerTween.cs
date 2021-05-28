adf553d8dbded791adf7b572b909fad00d87c853
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-28 21:27:00 +0700

    Add multi kill announcer 三

--- Assets/MultiKillAnnouncerTween.cs
+++ Assets/MultiKillAnnouncerTween.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 
 [RequireComponent(typeof(Transform), typeof(CanvasGroup))]
 
-public class MultiKillAnnouncerText : MonoBehaviour
+public class MultiKillAnnouncerTween : MonoBehaviour
 {
     [Header("Settings")]
     [Header("Pop-up")]
+    [SerializeField] private float scaleBefore;
+    [SerializeField] private float scaleAfter = 1f;
     [SerializeField] private float scaleDuration = 1f;
-    [SerializeField] private float scaleDelay = float.Epsilon;
+    [SerializeField] private float scaleDelay;
     [SerializeField] private Ease scaleEase = Ease.OutBounce;
     [SerializeField] private AnimationCurve scaleCustomCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
     [SerializeField] private bool scaleUseCustomCurve;
+
     [Header("Fade out")]
+    [SerializeField] private float fadeBefore = 1f;
+    [SerializeField] private float fadeAfter;
     [SerializeField] private float fadeDuration = 4f;
     [SerializeField] private float fadeDelay = 1f;
     [SerializeField] private Ease fadeEase = Ease.InExpo;
@@ -30,25 +35,25 @@ public class MultiKillAnnouncerText : MonoBehaviour
 
     private void OnEnable()
     {
-        _transform.localScale = new Vector3(float.Epsilon, float.Epsilon, float.Epsilon);
-        _canvasGroup.alpha = 1f;
+        _transform.localScale = new Vector3(scaleBefore, scaleBefore, scaleBefore);
+        _canvasGroup.alpha = fadeBefore;
         
         if (scaleUseCustomCurve)
         {
-            _transform.DOScale(new Vector3(1f, 1f, 1f), scaleDuration).SetDelay(scaleDelay).SetEase(scaleCustomCurve);
+            _transform.DOScale(new Vector3(scaleAfter, scaleAfter, scaleAfter), scaleDuration).SetDelay(scaleDelay).SetEase(scaleCustomCurve);
         }
         else
         {
-            _transform.DOScale(new Vector3(1f, 1f, 1f), scaleDuration).SetDelay(scaleDelay).SetEase(scaleEase);
+            _transform.DOScale(new Vector3(scaleAfter, scaleAfter, scaleAfter), scaleDuration).SetDelay(scaleDelay).SetEase(scaleEase);
         }
 
         if (fadeUseCustomCurve)
         {
-            _canvasGroup.DOFade(float.Epsilon, fadeDuration).SetDelay(fadeDelay).SetEase(fadeCustomCurve);
+            _canvasGroup.DOFade(fadeAfter, fadeDuration).SetDelay(fadeDelay).SetEase(fadeCustomCurve);
         }
         else
         {
-            _canvasGroup.DOFade(float.Epsilon, fadeDuration).SetDelay(fadeDelay).SetEase(fadeEase);
+            _canvasGroup.DOFade(fadeAfter, fadeDuration).SetDelay(fadeDelay).SetEase(fadeEase);
         }
     }
 }
