3d56ca86b294b8e572f3be2bff3b1c18408ea21b
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-28 19:03:15 +0700

    Add multi kill announcer

---
+++ Assets/MultiKillAnnouncer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MultiKillAnnouncer : MonoBehaviour
+{
+    [SerializeField] private GameObject doubleKill;
+    [SerializeField] private GameObject tripleKill;
+    [SerializeField] private GameObject quadrupleKill;
+    [SerializeField] private GameObject multiKill;
+
+    private void Awake()
+    {
+        HideAll();
+    }
+
+    public void Show(int numberOfKills)
+    {
+        HideAll();
+        
+        switch (numberOfKills)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                doubleKill.SetActive(true);
+                break;
+            case 3:
+                tripleKill.SetActive(true);
+                break;
+            case 4:
+                quadrupleKill.SetActive(true);
+                break;
+            default:
+                multiKill.SetActive(true);
+                break;
+        }
+    }
+
+    private void HideAll()
+    {
+        doubleKill.SetActive(false);
+        tripleKill.SetActive(false);
+        quadrupleKill.SetActive(false);
+        multiKill.SetActive(false);
+    }
+}
---
+++ Assets/MultiKillAnnouncerText.cs
@@ -0,0 +1,60 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform), typeof(CanvasGroup))]
+
+public class MultiKillAnnouncerText : MonoBehaviour
+{
+    [Header("Settings")]
+    [Header("Pop-up")]
+    [SerializeField] private float scaleDuration = 1f;
+    [SerializeField] private float scaleDelay = float.Epsilon;
+    [SerializeField] private Ease scaleEase = Ease.OutBounce;
+    [SerializeField] private AnimationCurve scaleCustomCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+    [SerializeField] private bool scaleUseCustomCurve;
+    [Header("Fade out")]
+    [SerializeField] private float fadeDuration = 4f;
+    [SerializeField] private float fadeDelay = 1f;
+    [SerializeField] private Ease fadeEase = Ease.InExpo;
+    [SerializeField] private AnimationCurve fadeCustomCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+    [SerializeField] private bool fadeUseCustomCurve;
+    
+    private Transform _transform;
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        
+        _transform.localScale = new Vector3(float.Epsilon, float.Epsilon, float.Epsilon);
+        _canvasGroup.alpha = 1f;
+    }
+
+    private void OnEnable()
+    {
+        if (scaleUseCustomCurve)
+        {
+            _transform.DOScale(new Vector3(1f, 1f, 1f), scaleDuration).SetDelay(scaleDelay).SetEase(scaleCustomCurve);
+        }
+        else
+        {
+            _transform.DOScale(new Vector3(1f, 1f, 1f), scaleDuration).SetDelay(scaleDelay).SetEase(scaleEase);
+        }
+
+        if (fadeUseCustomCurve)
+        {
+            _canvasGroup.DOFade(float.Epsilon, fadeDuration).SetDelay(fadeDelay).SetEase(fadeCustomCurve);
+        }
+        else
+        {
+            _canvasGroup.DOFade(float.Epsilon, fadeDuration).SetDelay(fadeDelay).SetEase(fadeEase);
+        }
+    }
+
+    private void OnDisable()
+    {
+        _transform.localScale = new Vector3(float.Epsilon, float.Epsilon, float.Epsilon);
+        _canvasGroup.alpha = 1f;
+    }
+}
