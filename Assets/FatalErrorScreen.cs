9c32b49e284c4b446d5248898232c0bd176b7303
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-28 08:15:49 +0700

    Add fatal error screen

---
+++ Assets/FatalErrorScreen.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FatalErrorScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject popup;
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text reason;
+
+    public void Show(string titleText, string reasonText)
+    {
+        title.text = titleText;
+        reason.text = reasonText;
+        popup.SetActive(true);
+    }
+    
+    public void Show(string reasonText)
+    {
+        title.text = "Fatal Error";
+        reason.text = reasonText;
+        popup.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
