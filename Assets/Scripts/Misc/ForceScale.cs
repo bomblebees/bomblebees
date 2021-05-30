dca061fec3c55438737158177adb6065a30407c3
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-30 07:05:45 +0700

    Prevent key bindings menu from randomly growing

---
+++ Assets/Scripts/Misc/ForceScale.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ForceScale : MonoBehaviour
+{
+    [SerializeField] private float scale = 1;
+    private Vector3 _scaleVector3;
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _scaleVector3 = new Vector3(scale, scale, scale);
+        _transform = GetComponent<Transform>();
+        _transform.localScale = _scaleVector3;
+    }
+
+    private void Update()
+    {
+        if (_transform.localScale.Equals(_scaleVector3)) return;
+
+        _transform.localScale = _scaleVector3;
+    }
+}
