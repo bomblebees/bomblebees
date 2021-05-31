925fbaf19656cb9c08c1efbe1811ac81305b6921
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-05-31 10:33:44 +0700

    Add script to modify multiple particle colors at once

---
+++ Assets/Scripts/Misc/ChangeAllParticleColors.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChangeAllParticleColors : MonoBehaviour
+{
+    [SerializeField] private Color color = new Color(1f, 0.65f, 0f);
+    private ParticleSystem[] _particleSystems;
+    private void Awake()
+    {
+        _particleSystems = GetComponentsInChildren<ParticleSystem>();
+
+        foreach (var system in _particleSystems)
+        {
+            var systemMain = system.main;
+            systemMain.startColor = color;
+        }
+    }
+}
