6e0d878bbad648f599b7fb953116ba191bbcad79
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-06-03 06:43:29 +0700

    Remove redundant 2nd/3rd/4th text on end screen

---
+++ Assets/Scripts/Misc/HideOnPlayerCount.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class HideOnPlayerCount : MonoBehaviour
+{
+    public enum HideMode
+    {
+        Disable,
+        Invisible,
+        Destroy
+    }
+    
+    public HideMode hideMode;
+    [Header("If Less Than:")]
+    [SerializeField] private int playerCount;
+
+    private void Awake()
+    {
+        if (FindObjectOfType<NetworkRoomManagerExt>().currentPlayers < playerCount)
+        {
+            HideThis();
+        }
+    }
+
+    private void HideThis()
+    {
+        if (hideMode == HideMode.Disable)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (hideMode == HideMode.Invisible)
+        {
+            if (!gameObject.GetComponent<CanvasGroup>().Equals(null))
+            {
+                gameObject.GetComponent<CanvasGroup>().alpha = float.Epsilon;
+            }
+            else
+            {
+                CanvasRenderer[] canvasRenderers = gameObject.GetComponentsInChildren<CanvasRenderer>();
+                foreach (var canvasRenderer in canvasRenderers)
+                {
+                    canvasRenderer.SetAlpha(float.Epsilon);
+                }
+            }
+        }
+        else if (hideMode == HideMode.Destroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
