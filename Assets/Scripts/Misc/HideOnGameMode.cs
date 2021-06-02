a9b541562171cf37a6b98d0d2573a58443d9e319
Iroha-Isshiki <irohai.isshiki@gmail.com>
2021-06-02 07:00:05 +0700

    Hide total points icon on standard game mode end screen

---
+++ Assets/Scripts/Misc/HideOnGameMode.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class HideOnGameMode : MonoBehaviour
+{
+    public enum HideMode
+    {
+        Disable,
+        Invisible,
+        Destroy
+    }
+
+    public enum GameMode
+    {
+        Standard,
+        ComboFrenzy
+    }
+
+    public HideMode hideMode;
+    public GameMode gameMode;
+
+    private void Awake()
+    {
+        if (gameMode == GameMode.Standard)
+        {
+            if (FindObjectOfType<LobbySettings>().GetGamemode() is StandardGamemode)
+            {
+                HideThis();
+            }
+        }
+        else if (gameMode == GameMode.ComboFrenzy)
+        {
+            if (FindObjectOfType<LobbySettings>().GetGamemode() is ComboGamemode)
+            {
+                HideThis();
+            }
+        }
+    }
+
+    private void HideThis()
+    {
+        if (hideMode == HideMode.Disable)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (hideMode == HideMode.Invisible)
+        {
+            if (!gameObject.GetComponent<CanvasGroup>().Equals(null))
+            {
+                gameObject.GetComponent<CanvasGroup>().alpha = float.Epsilon;
+            }
+            else
+            {
+                CanvasRenderer[] canvasRenderers = gameObject.GetComponentsInChildren<CanvasRenderer>();
+                foreach (var canvasRenderer in canvasRenderers)
+                {
+                    canvasRenderer.SetAlpha(float.Epsilon);
+                }
+            }
+        }
+        else if (hideMode == HideMode.Destroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
